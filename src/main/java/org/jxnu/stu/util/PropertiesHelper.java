a7aaef56bf63d845c000f6c34c4563ec45d24396
shaohengwei <296017678@qq.com>
2019-06-22 18:17:31 +0800

    Maven环境隔离

--- src/main/java/org/jxnu/stu/util/PropertiesHelper.java
+++ src/main/java/org/jxnu/stu/util/PropertiesHelper.java
@@ -3,12 +3,25 @@ package org.jxnu.stu.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ResourceBundle;
 
 @Component
 public class PropertiesHelper {
 
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("application-dev");
+    private static String profile;
+
+    private static ResourceBundle resourceBundle = null;
+
+    @Value("${spring.profiles.active}")
+    public void setProfile(String profile){
+        PropertiesHelper.profile = profile;
+    }
+
+    @PostConstruct
+    private void initResourceBundle(){
+        resourceBundle = ResourceBundle.getBundle("application-" + profile);
+    }
 
     public static String getProperties(String key){
         return resourceBundle.getString(key);
